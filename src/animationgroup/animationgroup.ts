0c7e86ec0450bfc33c4f3b6764d45245c23d9b23
niuee <vntchang@gmail.com>
2023-12-26 17:41:20 +0800

    Add another AnimationGroup class denoted as AnimationGroupB to have a time based staggered animation

--- src/animationgroup/animationgroup.ts
+++ src/animationgroup/animationgroup.ts
@@ -19,6 +19,17 @@ export type AnimationSequence<T> = {
     tearDown?: Function;
 }
 
+export type AnimationSeq<T> = {
+    startTime?: number; // this is the time in parent group
+    duration: number;
+    keyframes: Keyframe<T>[];
+    applyAnimationValue: (value: T) => void;
+    animatableAttributeHelper: AnimatableAttributeHelper<T>;
+    easeFn?: (percentage: number) => number;
+    setUp?: Function;
+    tearDown?: Function;
+}
+
 export class AnimationGroup{
 
     private timePercentage: number;
@@ -139,4 +150,122 @@ export class AnimationGroup{
         }
     }
 
+}
+
+export class AnimationGroupB{
+
+    private startTime: number; // this is the time in parent group
+    private localTime: number; // local time starting from 0 up til duration
+    private duration: number;
+    private keyframesList: AnimationSeq<any>[];
+    private currentKeyframeIndex: number[];
+    private loop: boolean;
+    private onGoing: boolean;
+
+    constructor(startTime: number = 0, keyframes: AnimationSeq<any>[], duration: number = 1, loop: boolean = false){
+        this.startTime = startTime;
+        this.localTime = duration + 0.1;
+        this.duration = duration;
+        this.keyframesList = keyframes;
+        this.loop = loop;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.onGoing = false;
+    }
+
+    startAnimation(){
+        this.localTime = 0;
+        this.onGoing = true;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.keyframesList.forEach((animationSequence, index) => {
+            if(animationSequence.setUp != undefined){
+                animationSequence.setUp();
+            }
+        });
+        this.animate(0);
+    }
+
+    cancelAnimation(){
+        this.localTime = this.duration + 0.1;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.onGoing = false;
+        this.keyframesList.forEach((animationSequence, index) => {
+            if(animationSequence.tearDown != undefined){
+                animationSequence.tearDown();
+            }
+        });
+    }
+
+    pauseAnimation(){
+        this.onGoing = false;
+    }
+
+    resumeAnimation(){
+        this.onGoing = true;
+    }
+
+    // delta time should be in seconds
+    animate(deltaTime: number){
+        if(this.localTime <= this.duration && this.onGoing){
+            this.localTime += deltaTime;
+            for(let index = 0; index < this.keyframesList.length; index++){
+                const animationSequence = this.keyframesList[index];
+                if(animationSequence.startTime != undefined && animationSequence.startTime > this.localTime){
+                    continue;
+                }
+                if(animationSequence.startTime == undefined){
+                    animationSequence.startTime = this.startTime;
+                }
+                if (animationSequence.easeFn == undefined){
+                    animationSequence.easeFn = easeFunctions.easeInOutSine;
+                }
+                let localDeltaTimePercentage = (this.localTime - animationSequence.startTime) / animationSequence.duration;
+                let targetPercentage = animationSequence.easeFn(localDeltaTimePercentage);
+                if (localDeltaTimePercentage > 1){
+                    targetPercentage = animationSequence.easeFn(1);
+                }
+                const curFrameNumber = this.currentKeyframeIndex[index];
+                let value: any;
+                if(curFrameNumber < animationSequence.keyframes.length && animationSequence.keyframes[curFrameNumber].percentage == targetPercentage){
+                    value = animationSequence.keyframes[curFrameNumber].value;
+                } else {
+                    value = this.findValue(targetPercentage, animationSequence.keyframes, animationSequence.animatableAttributeHelper);
+                }
+                while(this.currentKeyframeIndex[index] < animationSequence.keyframes.length && animationSequence.keyframes[this.currentKeyframeIndex[index]].percentage <= targetPercentage){
+                    this.currentKeyframeIndex[index] += 1;
+                }
+                animationSequence.applyAnimationValue(value);
+            }
+            if(this.localTime > this.duration && this.loop){
+                this.localTime = 0;
+            }
+        }
+    }
+
+    findValue(valuePercentage: number, keyframes: Keyframe<any>[], animatableAttributeHelper: AnimatableAttributeHelper<any>): any{
+        if(valuePercentage > 1){
+            return animatableAttributeHelper.lerp(valuePercentage, keyframes[keyframes.length - 2], keyframes[keyframes.length - 1]);
+        }
+        if(valuePercentage < 0){
+            return animatableAttributeHelper.lerp(valuePercentage, keyframes[1], keyframes[0]);
+        }
+        let left = 0;
+        let right = keyframes.length - 1;
+        while (left <= right) {
+            let mid = left + Math.floor((right - left) / 2);
+            if(keyframes[mid].percentage == valuePercentage) {
+                return keyframes[mid].value;
+            } else if(keyframes[mid].percentage < valuePercentage){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return animatableAttributeHelper.lerp(valuePercentage, keyframes[left - 1], keyframes[left]);
+    }
+
+    setDuration(duration: number){
+        if(!this.onGoing && this.localTime > this.duration){
+            this.duration = duration;
+        }
+    }
 }
--- tests/animator.test.ts
+++ tests/animator.test.ts
@@ -1,5 +1,5 @@
 import { Point } from "point2point";
-import { AnimationSequence, Keyframe, PointAnimationHelper, AnimationGroup} from "../src";
+import { AnimationSequence, Keyframe, PointAnimationHelper, AnimationGroup, AnimationGroupB, AnimationSeq} from "../src";
 import * as EasingFunctions from "../src/easeFunctions";
 
 export class AnimationMockClass {
@@ -38,6 +38,94 @@ describe("Animator Tests", ()=>{
             animatableAttributeHelper: new PointAnimationHelper(),
         };
         testAnimator = new AnimationGroup([positionAnimationSequence]);
+    });
+
+    test("Play Animation", ()=>{
+        const duration = 2;
+        testAnimator.setDuration(duration);
+        testAnimator.startAnimation();
+        const deltaTime = 0.1;
+        let time = 0;
+        while (time <= duration){
+            testAnimator.animate(deltaTime);
+            time += deltaTime;
+        }
+        expect(exampleObj.getPosition()).toEqual({x: 10, y: 10});
+    });
+
+    test("Without starting an animation the animated attribute won't change", ()=>{
+        const deltaTime = 0.1;
+        let time = 0;
+        while (time <= 1){
+            testAnimator.animate(deltaTime);
+            time += deltaTime;
+        }
+        expect(exampleObj.getPosition()).toEqual({x: 0, y: 0});
+    });
+
+    test("Canceling the animation would stop the animation", ()=>{
+        const deltaTime = 0.1;
+        let time = 0;
+        testAnimator.startAnimation();
+        let expectedPosition: Point = {x: 0, y: 0};
+        while (time <= 1){
+            if(time == 0.4){
+                testAnimator.cancelAnimation();
+                expectedPosition = exampleObj.getPosition();
+            }
+            testAnimator.animate(deltaTime);
+            time += deltaTime;
+        }
+        expect(exampleObj.getPosition()).toEqual(expectedPosition);
+    });
+
+    test("Animation is played according to keyframes", ()=>{
+        const deltaTime = 0.1;
+        let time = 0;
+        testAnimator.startAnimation();
+        while (time <= 1){
+            if(time == 0.5){
+                expect(exampleObj.getPosition().x).toBeCloseTo(3);
+                expect(exampleObj.getPosition().y).toBeCloseTo(3);
+            }
+            testAnimator.animate(deltaTime);
+            time += deltaTime;
+        }
+    });
+
+    test("Animation that will use the extrapolation in helper", ()=>{
+        const deltaTime = 0.01;
+        let time = 0;
+        testAnimator.startAnimation();
+        while (time <= 1){
+            testAnimator.animate(deltaTime);
+            time += deltaTime;
+        }
+    });
+
+});
+
+describe("AnimationGroupB Tests", ()=>{
+
+    let testAnimator: AnimationGroupB;
+    let exampleObj: AnimationMockClass;
+
+    beforeEach(()=>{
+
+        const positionKeyframes: Keyframe<Point>[] = [];
+
+        positionKeyframes.push({percentage: 0, value: {x: 0, y: 0}});
+        positionKeyframes.push({percentage: 0.5, value: {x: 3, y: 3}});
+        positionKeyframes.push({percentage: 1, value: {x: 10, y: 10}});
+
+        exampleObj = new AnimationMockClass({x: 0, y: 0});
+        const positionAnimationSequence: AnimationSeq<Point> = {
+            duration: 1,
+            keyframes: positionKeyframes,
+            applyAnimationValue: (value: Point) => {exampleObj.setPosition(value);},
+            animatableAttributeHelper: new PointAnimationHelper(),
+        };
+        testAnimator = new AnimationGroupB(0, [positionAnimationSequence], 1, false);
     });
 
     test("Play Animation", ()=>{
