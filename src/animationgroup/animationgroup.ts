e313c0729e4db7627c2d1470f60878756079631c
niuee <vntchang@gmail.com>
2023-12-27 13:25:37 +0800

    Rearrange the order of legacy code

--- src/animationgroup/animationgroup.ts
+++ src/animationgroup/animationgroup.ts
@@ -6,19 +6,6 @@ export type Keyframe<T> = {
     value: T;
 }
 
-export type AnimationSequenceLegacy<T> = {
-    stagger?: {
-        startPercentage: number;
-        keyframePercentageIsRelative: boolean; // relative will gurantee to play the entire animation sequence from 0 percentage; // if not relative, the animation sequence will be played at the starting percentage. 
-    }
-    keyframes: Keyframe<T>[];
-    applyAnimationValue: (value: T) => void;
-    animatableAttributeHelper: AnimatableAttributeHelper<T>;
-    easeFn?: (percentage: number) => number;
-    setUp?: Function;
-    tearDown?: Function;
-}
-
 export type AnimationSequence<T> = {
     startTime?: number; // this is the time in parent group
     duration: number;
@@ -28,128 +15,6 @@ export type AnimationSequence<T> = {
     easeFn?: (percentage: number) => number;
     setUp?: Function;
     tearDown?: Function;
-}
-
-export class AnimationGroupLegacy{
-
-    private timePercentage: number;
-    private duration: number;
-    private keyframesList: AnimationSequenceLegacy<any>[];
-    private currentKeyframeIndex: number[];
-    private loop: boolean;
-    private currentTime: number;
-    private onGoing: boolean;
-
-    constructor(keyframes: AnimationSequenceLegacy<any>[], duration: number = 1, loop: boolean = false){
-        this.timePercentage = 1.1;
-        this.duration = duration;
-        this.keyframesList = keyframes;
-        this.loop = loop;
-        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
-        this.currentTime = 0;
-        this.onGoing = false;
-    }
-
-    startAnimation(){
-        this.timePercentage = 0;
-        this.currentTime = 0;
-        this.onGoing = true;
-        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
-        this.keyframesList.forEach((animationSequence, index) => {
-            if(animationSequence.setUp != undefined){
-                animationSequence.setUp();
-            }
-        });
-    }
-
-    cancelAnimation(){
-        this.timePercentage = 1.1;
-        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
-        this.onGoing = false;
-        this.currentTime = 0;
-        this.keyframesList.forEach((animationSequence, index) => {
-            if(animationSequence.tearDown != undefined){
-                animationSequence.tearDown();
-            }
-        });
-    }
-
-    pauseAnimation(){
-        this.onGoing = false;
-    }
-
-    resumeAnimation(){
-        this.onGoing = true;
-    }
-
-    // delta time should be in seconds
-    animate(deltaTime: number){
-        if(this.timePercentage <= 1 && this.onGoing){
-            this.currentTime += deltaTime;
-            let currentDeltaTimePercentage = deltaTime / this.duration;
-            let targetTimePercentage = this.timePercentage + currentDeltaTimePercentage;
-            for(let index = 0; index < this.keyframesList.length; index++){
-                const animationSequence = this.keyframesList[index];
-                if(animationSequence.stagger != undefined && animationSequence.stagger.startPercentage > targetTimePercentage){
-                    continue;
-                }
-                if (animationSequence.easeFn == undefined){
-                    animationSequence.easeFn = easeFunctions.easeInOutSine;
-                }
-                let targetPercentage = animationSequence.easeFn(targetTimePercentage);
-                if (animationSequence.stagger != undefined && animationSequence.stagger.keyframePercentageIsRelative){
-                    targetPercentage =  animationSequence.easeFn((targetTimePercentage - animationSequence.stagger.startPercentage) / (1 - animationSequence.stagger.startPercentage));
-                }
-                if (targetTimePercentage > 1){
-                    targetPercentage = animationSequence.easeFn(1);
-                }
-                const curFrameNumber = this.currentKeyframeIndex[index];
-                let value: any;
-                if(curFrameNumber < animationSequence.keyframes.length && animationSequence.keyframes[curFrameNumber].percentage == targetPercentage){
-                    value = animationSequence.keyframes[curFrameNumber].value;
-                } else {
-                    value = this.findValue(targetPercentage, animationSequence.keyframes, animationSequence.animatableAttributeHelper);
-                }
-                while(this.currentKeyframeIndex[index] < animationSequence.keyframes.length && animationSequence.keyframes[this.currentKeyframeIndex[index]].percentage <= targetPercentage){
-                    this.currentKeyframeIndex[index] += 1;
-                }
-                animationSequence.applyAnimationValue(value);
-            }
-            this.timePercentage = targetTimePercentage;
-            if(this.timePercentage > 1 && this.loop){
-                this.timePercentage = 0;
-            }
-        }
-    }
-
-    findValue(valuePercentage: number, keyframes: Keyframe<any>[], animatableAttributeHelper: AnimatableAttributeHelper<any>): any{
-        if(valuePercentage > 1){
-            return animatableAttributeHelper.lerp(valuePercentage, keyframes[keyframes.length - 2], keyframes[keyframes.length - 1]);
-        }
-        if(valuePercentage < 0){
-            return animatableAttributeHelper.lerp(valuePercentage, keyframes[1], keyframes[0]);
-        }
-        let left = 0;
-        let right = keyframes.length - 1;
-        while (left <= right) {
-            let mid = left + Math.floor((right - left) / 2);
-            if(keyframes[mid].percentage == valuePercentage) {
-                return keyframes[mid].value;
-            } else if(keyframes[mid].percentage < valuePercentage){
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return animatableAttributeHelper.lerp(valuePercentage, keyframes[left - 1], keyframes[left]);
-    }
-
-    setDuration(duration: number){
-        if(!this.onGoing && this.currentTime == 0){
-            this.duration = duration;
-        }
-    }
-
 }
 
 export class AnimationGroup{
@@ -310,4 +175,139 @@ export class AnimationGroup{
             animationSequence.startTime += time;
         });
     }
+}
+
+export type AnimationSequenceLegacy<T> = {
+    stagger?: {
+        startPercentage: number;
+        keyframePercentageIsRelative: boolean; // relative will gurantee to play the entire animation sequence from 0 percentage; // if not relative, the animation sequence will be played at the starting percentage. 
+    }
+    keyframes: Keyframe<T>[];
+    applyAnimationValue: (value: T) => void;
+    animatableAttributeHelper: AnimatableAttributeHelper<T>;
+    easeFn?: (percentage: number) => number;
+    setUp?: Function;
+    tearDown?: Function;
+}
+
+export class AnimationGroupLegacy{
+
+    private timePercentage: number;
+    private duration: number;
+    private keyframesList: AnimationSequenceLegacy<any>[];
+    private currentKeyframeIndex: number[];
+    private loop: boolean;
+    private currentTime: number;
+    private onGoing: boolean;
+
+    constructor(keyframes: AnimationSequenceLegacy<any>[], duration: number = 1, loop: boolean = false){
+        this.timePercentage = 1.1;
+        this.duration = duration;
+        this.keyframesList = keyframes;
+        this.loop = loop;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.currentTime = 0;
+        this.onGoing = false;
+    }
+
+    startAnimation(){
+        this.timePercentage = 0;
+        this.currentTime = 0;
+        this.onGoing = true;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.keyframesList.forEach((animationSequence, index) => {
+            if(animationSequence.setUp != undefined){
+                animationSequence.setUp();
+            }
+        });
+    }
+
+    cancelAnimation(){
+        this.timePercentage = 1.1;
+        this.currentKeyframeIndex = Array(this.keyframesList.length).fill(0);
+        this.onGoing = false;
+        this.currentTime = 0;
+        this.keyframesList.forEach((animationSequence, index) => {
+            if(animationSequence.tearDown != undefined){
+                animationSequence.tearDown();
+            }
+        });
+    }
+
+    pauseAnimation(){
+        this.onGoing = false;
+    }
+
+    resumeAnimation(){
+        this.onGoing = true;
+    }
+
+    // delta time should be in seconds
+    animate(deltaTime: number){
+        if(this.timePercentage <= 1 && this.onGoing){
+            this.currentTime += deltaTime;
+            let currentDeltaTimePercentage = deltaTime / this.duration;
+            let targetTimePercentage = this.timePercentage + currentDeltaTimePercentage;
+            for(let index = 0; index < this.keyframesList.length; index++){
+                const animationSequence = this.keyframesList[index];
+                if(animationSequence.stagger != undefined && animationSequence.stagger.startPercentage > targetTimePercentage){
+                    continue;
+                }
+                if (animationSequence.easeFn == undefined){
+                    animationSequence.easeFn = easeFunctions.easeInOutSine;
+                }
+                let targetPercentage = animationSequence.easeFn(targetTimePercentage);
+                if (animationSequence.stagger != undefined && animationSequence.stagger.keyframePercentageIsRelative){
+                    targetPercentage =  animationSequence.easeFn((targetTimePercentage - animationSequence.stagger.startPercentage) / (1 - animationSequence.stagger.startPercentage));
+                }
+                if (targetTimePercentage > 1){
+                    targetPercentage = animationSequence.easeFn(1);
+                }
+                const curFrameNumber = this.currentKeyframeIndex[index];
+                let value: any;
+                if(curFrameNumber < animationSequence.keyframes.length && animationSequence.keyframes[curFrameNumber].percentage == targetPercentage){
+                    value = animationSequence.keyframes[curFrameNumber].value;
+                } else {
+                    value = this.findValue(targetPercentage, animationSequence.keyframes, animationSequence.animatableAttributeHelper);
+                }
+                while(this.currentKeyframeIndex[index] < animationSequence.keyframes.length && animationSequence.keyframes[this.currentKeyframeIndex[index]].percentage <= targetPercentage){
+                    this.currentKeyframeIndex[index] += 1;
+                }
+                animationSequence.applyAnimationValue(value);
+            }
+            this.timePercentage = targetTimePercentage;
+            if(this.timePercentage > 1 && this.loop){
+                this.timePercentage = 0;
+            }
+        }
+    }
+
+    findValue(valuePercentage: number, keyframes: Keyframe<any>[], animatableAttributeHelper: AnimatableAttributeHelper<any>): any{
+        if(valuePercentage > 1){
+            return animatableAttributeHelper.lerp(valuePercentage, keyframes[keyframes.length - 2], keyframes[keyframes.length - 1]);
+        }
+        if(valuePercentage < 0){
+            return animatableAttributeHelper.lerp(valuePercentage, keyframes[1], keyframes[0]);
+        }
+        let left = 0;
+        let right = keyframes.length - 1;
+        while (left <= right) {
+            let mid = left + Math.floor((right - left) / 2);
+            if(keyframes[mid].percentage == valuePercentage) {
+                return keyframes[mid].value;
+            } else if(keyframes[mid].percentage < valuePercentage){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return animatableAttributeHelper.lerp(valuePercentage, keyframes[left - 1], keyframes[left]);
+    }
+
+    setDuration(duration: number){
+        if(!this.onGoing && this.currentTime == 0){
+            this.duration = duration;
+        }
+    }
+
 }
